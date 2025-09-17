d4259e1ad67bbc7f1c85bc077e9c5b616fe774c8
hinaazubairr <hinaa.zubairr@gmail.com>
2025-09-17 17:56:51 +0300

    Project to calculate AREA OF A SURFACE

---
+++ Project-to-calculate-AREA.py
@@ -0,0 +1,13 @@
+# SECTION # 2 : TUTORIAL ON : HOW TO CREATE A VARIABLE
+# SECTION # 2 : EXAMPLE-7
+
+# Define a variable names ‘area’ that multiply the value of ‘length’ variable and ‘width’ variable. 
+# And print the result on the screen.
+
+length = 10
+width = 10
+
+
+area = length*width
+
+print('Area:', area)
