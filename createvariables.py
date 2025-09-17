1ab9be760a87c723b546805af7d53502229640c3
hinaazubairr <hinaa.zubairr@gmail.com>
2025-09-17 17:49:01 +0300

    Examples for creating variables in Python

--- createvariables.py
+++ createvariables.py
@@ -1,6 +1,50 @@
+# SECTION # 1 : TUTORIAL ON : HOW TO ADD COMMENTS IN PYTHON
+# TUTORIAL ON : HOW TO ADD COMMENTS IN PYTHON
+
 # we use HASHTAG to write 
 #       #	Single line comment
 	
 # we use ''' to add multi line comments
 ##
+
+
+
+# SECTION # 2 : TUTORIAL ON : HOW TO CREATE A VARIABLE
+
+# SECTION # 2 : EXAMPLE-1
+
+a = 100
+print ('New variable has the value:', a )
+
+
+
+# SECTION # 2 : EXAMPLE-2
+
+name = 'Hina Zubair'
+print(name)
+
+
+# SECTION # 2 : EXAMPLE-3
+# Assign number 9 to a variable named ‘start’.
+
+start = 9
+print(start)
+
+
+# SECTION # 2 : EXAMPLE-4
+# Define a variable ‘p’ and make it refer to 9341.
+p = '9341'
+
+
+
+# SECTION # 2 : EXAMPLE-5
+# Assign number 8 to a variable named ‘e’.
+e = 8
+
+
+# SECTION # 2 : EXAMPLE-6
+# Define two variables, one named ‘length’ with value refer to 15 and the other named ‘width’ refer to 1.55.
+length =5
+width =1.55
+
 
